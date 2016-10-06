14fb17e8e7d026e1a52bdecb3c122924fc08bf10
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-10-06 19:58:25 +0300

    js для тестовой страницы

---
+++ blocks/dev-page/_type/dev-page_type_test.js
@@ -0,0 +1,21 @@
+// Добавляет в window интерфейсы для описания тестов (describe и пр.)
+if (typeof window.initMochaPhantomJS === 'function') {
+    window.initMochaPhantomJS();
+}
+mocha.ui('bdd');
+mocha.setup({ timeout: 10000 });
+
+(function() {
+    var origCallPhantom = window.callPhantom;
+
+    // хак для запуска бандла блока без тестов https://github.com/nathanboktae/mocha-phantomjs-core/issues/5
+    // без него выбрасывается исключение "mocha.run() was called with no tests"
+    window.callPhantom = function(arg) {
+        if (arg && typeof arg.testRunStarted !== 'undefined') {
+            arg.testRunStarted === 0 && (arg.testRunStarted = 1);
+            window.callPhantom = origCallPhantom;
+        }
+
+        origCallPhantom(arg);
+    };
+})();
